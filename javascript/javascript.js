72061490973fc7f6dc897ef1d9a9d7bc1c5236a7
kunstaytrue <st10487104@rcconnect.edu.za>
2025-11-17 21:32:17 +0200

    Update javascript.js
    this is the logic for the accordian implemented in service page

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -48,3 +48,16 @@ document.addEventListener("DOMContentLoaded", function () {
       navLinks.classList.toggle("active");
     });
   }
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+  accordionHeaders.forEach(header => {
+    header.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
