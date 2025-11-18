3aa5a06511bad56fcaf25415025454933bba8259
kunstaytrue <st10487104@rcconnect.edu.za>
2025-11-18 19:15:45 +0200

    Making pages more dynamic
    Okay so some major changes occurred. I implemented a lot of hover movements as well as fades in order to make all the pages more dynamic so I had to make changes on my JavaScript as well as my CSS and also implement the different classes in all the pages in order for the fades and models to work properly. Now every single page is way more dynamic. Yes that is about it.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -242,3 +242,20 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 });
+const animatedElements = document.querySelectorAll(
+  ".fade-in, .fade-in-up, .fade-in-left, .fade-in-right"
+);
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
+
